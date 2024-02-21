35107a2c3d142ce1b1ccae4f1cb596e5f2c19d95
Hector Puentes <hectorjosepuentesruiz@gmail.com>
2024-02-20 19:00:25 -0500

    tsting

--- Interface/main.py
+++ Interface/main.py
@@ -1,4 +1,13 @@
 from flask import Flask,render_template,request,redirect,url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField,SubmitField
+from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+
+#Create a flask instance
+
 
 app = Flask(__name__)
 
@@ -6,6 +15,10 @@ app = Flask(__name__)
 def index():
     title = 'Home'
     return render_template('index.html',title=title)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 @app.route('/about')
 def about():
@@ -17,10 +30,21 @@ def contact():
     title = 'Contact'
     return render_template('contact.html',title=title)
 
-@app.route('/blog')
+@app.route('/dashboard')
+def dashboard():
+    title = 'Dashboard'
+    return render_template('dashboard.html',title=title)
+
+@app.route('/blogs')
 def blog():
-    title = 'Blog'
+    title = 'Blogs'
     return render_template('blog.html',title=title)
+
+@app.route('/blog/<int:id>/')
+def post(id):
+    title = 'Blog'
+
+    return render_template('post.html',title=title)
 
 
 if __name__ == "__main__":
