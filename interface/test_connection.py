68ff92832d10afbb7650af32354ef7a2af0838ff
Hector Puentes <hectorjosepuentesruiz@gmail.com>
2024-03-26 21:56:41 -0500

    adding communication testing files

--- interface/test_connection.py
+++ interface/test_connection.py
@@ -1,11 +1,29 @@
-import socket as sockets
-s=sockets.socket(sockets.AF_INET,sockets.SOCK_STREAM)
-s.bind((sockets.gethostname(),1234))
-print(sockets.gethostname())
-s.listen(5)
+#Se importa el módulo
+import socket
+
+#instanciamos un objeto para trabajar con el socket
+ser = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#Puerto y servidor que debe escuchar
+ser.bind(("", 8050))
+
+#Aceptamos conexiones entrantes con el metodo listen. Por parámetro las conexiones simutáneas.
+ser.listen(1)
+
+#Instanciamos un objeto cli (socket cliente) para recibir datos
+cli, addr = ser.accept()
 
 while True:
-    clientsocket, address = s.accept()
-    print(f"Connection from {address} has been established!")
-    clientsocket.send(bytes("Welcome to the server!", "utf-8"))
-    clientsocket.close()
+    #Recibimos el mensaje, con el metodo recv recibimos datos. Por parametro la cantidad de bytes para recibir
+    recibido = cli.recv(1024)
+
+    #Si se reciben datos nos muestra la IP y el mensaje recibido
+    print("Recibo conexion de la IP: " + str(addr[0]) + " Puerto: " + str(addr[1]))
+    print(recibido)
+    #Devolvemos el mensaje al cliente
+    #cli.send(“mensaje recibido”)
+    msg_toSend=("Mensaje recibido!!")
+    cli.send(msg_toSend.encode('ascii'))
+
+#Cerramos la instancia del socket cliente y servidor
+cli.close()
