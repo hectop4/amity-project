323326d221eaf3356c3e149b5f1091ef421ed6c9
Hector Puentes <hectorjosepuentesruiz@gmail.com>
2024-04-15 22:05:26 -0500

    fin del dia

---
+++ interface/servidor/data_generator.py
@@ -0,0 +1,15 @@
+import random
+
+def generator():
+    data_dict={}
+    dt=random.randint(1, 100)
+    dp=random.randint(1, 100)
+    dw=random.randint(1, 100)
+    dh=random.randint(1, 100)
+    dhu=random.randint(1, 100)
+    data_dict['T']=dt
+    data_dict['P']=dp
+    data_dict['W']=dw
+    data_dict['H']=dh
+    data_dict['HU']=dhu
+    return data_dict
---
+++ interface/servidor/main.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+import data_generator as dg
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    print(dg.generator())
+    app.run('127.0.0.1',5000,debug=True)
+
+    
