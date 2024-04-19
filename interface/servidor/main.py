af5e6c1c0259e602b6a732b7ab33b3172c8f4633
Hector Puentes <hectorjosepuentesruiz@gmail.com>
2024-04-19 09:14:19 -0500

    rover

--- interface/servidor/main.py
+++ interface/servidor/main.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template
 import data_generator as dg
+from flask_socketio import send, emit, SocketIO
+
+
 app = Flask(__name__)
+socketio = SocketIO(app)
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',data=dg.generator())
+
+@socketio.on('json')
+def handle_json(json):
+    send(json, json=True)
+
 
 if __name__ == '__main__':
     print(dg.generator())
